acae43bc42e0f21cc158fb5f8456fa306c1ecf5e
Andrey Morokhin <morokhin.andrei@gmail.com>
2024-10-04 12:15:57 +0500

    Add Task 1 solution

--- src/main/java/ru/naumen/collection/task1/Task1.java
+++ src/main/java/ru/naumen/collection/task1/Task1.java
@@ -1,5 +1,8 @@
 package ru.naumen.collection.task1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Дано:
  * <pre>
@@ -24,6 +27,8 @@ package ru.naumen.collection.task1;
  */
 public class Task1
 {
+    private final Map<Ticket, Goods> ticketToGoods = new HashMap<>();
+
     public enum Goods {
         /**
          * нет товаров
@@ -47,7 +52,6 @@ public class Task1
      * <p>Достаточно их определить только для id, т.к. он уникален</p>
      */
     public Goods getGoods(Ticket ticket) {
-        // TODO реализовать
-        return null;
+        return ticketToGoods.get(ticket);
     }
 }
--- src/main/java/ru/naumen/collection/task1/Ticket.java
+++ src/main/java/ru/naumen/collection/task1/Ticket.java
@@ -9,4 +9,17 @@ package ru.naumen.collection.task1;
 public class Ticket {
     private long id;
     private String client;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        Ticket objectTicket = (Ticket) obj;
+        return objectTicket.id == id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.valueOf(id).hashCode();
+    }
 }
