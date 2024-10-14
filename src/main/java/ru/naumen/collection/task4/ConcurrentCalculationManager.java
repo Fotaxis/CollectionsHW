4f736c6b77426f666699405a695c4a1f83c5b737
Soldatov Aleksey <Fotaxis@mail.ru>
2024-10-14 13:08:32 +0500

    Solution

--- src/main/java/ru/naumen/collection/task4/ConcurrentCalculationManager.java
+++ src/main/java/ru/naumen/collection/task4/ConcurrentCalculationManager.java
@@ -1,25 +1,33 @@
 package ru.naumen.collection.task4;
 
+import java.util.concurrent.*;
 import java.util.function.Supplier;
 
 /**
  * Класс управления расчётами
  */
-public class ConcurrentCalculationManager<T> {
+    public class ConcurrentCalculationManager<T> {
 
-    /**
-     * Добавить задачу на параллельное вычисление
-     */
-    public void addTask(Supplier<T> task) {
-        // TODO реализовать
-    }
+            private final BlockingQueue<Future<T>> tasks = new LinkedBlockingQueue<>();
+            private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-    /**
-     * Получить результат вычисления.
-     * Возвращает результаты в том порядке, в котором добавлялись задачи.
-     */
-    public T getResult() {
-        // TODO реализовать
-        return null;
+        /**
+         * Добавить задачу на параллельное вычисление
+         */
+        public void addTask(Supplier<T> task) {
+            Future<T> future = executorService.submit(task::get);
+            tasks.add(future);
+        }
+
+        /**
+         * Получить результат вычисления.
+         * Возвращает результаты в том порядке, в котором добавлялись задачи.
+         */
+        public T getResult() {
+            try {
+                return tasks.take().get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-}
