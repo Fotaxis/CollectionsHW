967404c0fb36fb60cd8f40b656e71a5fbb0dcb34
Soldatov Aleksey <Fotaxis@mail.ru>
2024-10-14 12:58:34 +0500

    Change hashMap to LinkedHashMap, complexity recalculation

--- src/main/java/ru/naumen/collection/task3/WarAndPeace.java
+++ src/main/java/ru/naumen/collection/task3/WarAndPeace.java
@@ -22,46 +22,45 @@ public class WarAndPeace
             "Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt");
 
     public static void main(String[] args) {
-        Map<String, Integer> wordsToCount = new HashMap<>();
-        // Можно было использовать  LinkedHashMap для ускорения итерирования, но будет затратнее по памяти
+        Map<String, Integer> wordsToCount = new LinkedHashMap<>();
+        // LinkedHashMap для ускорения итерирования, Map для хранения и быстрого доступа к количеству повторений у слова
 
-        AbstractQueue<Map.Entry<String, Integer>> mostFreqWords = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
-        AbstractQueue<Map.Entry<String, Integer>> leastFreqWords = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        Queue<Map.Entry<String, Integer>> mostFreqWords = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        Queue<Map.Entry<String, Integer>> leastFreqWords = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
         // Для быстрого отбрасывания лишних элементов
 
         new WordParser(WAR_AND_PEACE_FILE_PATH) // O(n) где n кол-во слов
-                .forEachWord(word -> {
-                    if (wordsToCount.containsKey(word))
-                        wordsToCount.put(word, wordsToCount.get(word) + 1);
-                    else
-                        wordsToCount.put(word, 1);
-                });
+                .forEachWord(word -> wordsToCount.merge(word, 1, Integer::sum));
         // O(n), где n кол-во слов
 
-        for (Map.Entry<String, Integer> wordToCount: wordsToCount.entrySet()) {
-            mostFreqWords.add(wordToCount);
-            if (mostFreqWords.size() > 10)
-                mostFreqWords.poll();
-            leastFreqWords.add(wordToCount);
-            if (leastFreqWords.size() > 10)
-                leastFreqWords.poll();
+        for (Map.Entry<String, Integer> wordToCount: wordsToCount.entrySet()) { //O(n + s) для перебора hashMap (n - кол-во слов в hashmap, s - capacity у hashmap)
+            mostFreqWords.add(wordToCount); // O(log10), т.к. максимум 10 слов в очереди -> ~3
+            if (mostFreqWords.size() > 10)  // O(1)
+                mostFreqWords.poll(); // O(log10), т.к. максимум 10 слов в очереди -> ~3
+            leastFreqWords.add(wordToCount); // O(log10), т.к. максимум 10 слов в очереди -> ~3
+            if (leastFreqWords.size() > 10) //O(1)
+                leastFreqWords.poll(); // O(log10), т.к. максимум 10 слов в очереди -> ~3
         }
-        //O(n + s) для перебора hashMap (n - кол-во слов в hashmap, s - capacity у hashmap), 
-        // size -> O(1), add(poll) - O(log(10) ~ 3) т.к. максимум 10 слов
-        // итого -> O((n+s) * 3 * 2) = O(6n + 6s) или O(n)
+        // итого -> O((n+s) * 4) = O(4n + 4s) или O(n)
 
         System.out.println("TOP 10 наиболее используемых слов:");
-        mostFreqWords.stream()
-                .sorted((a, b) -> b.getValue() - a.getValue())
+        getOrderedQueue(mostFreqWords)
                 .forEach((entry) -> System.out.printf("%s - %d раз(а)\n", entry.getKey(), entry.getValue()));
-        //(O(10log10) + 10) ~43 т.к. в очереди максимум 10 слов, сложность сортировки nlogn + итерация по элементам
-
+        //(O(10log10) + 10) ~43 т.к. 10log10 у функции getOrderedQueue + 10 на итерацию или O(1)
         System.out.println("LAST 10 наименее используемых:");
-        leastFreqWords.stream()
-                .sorted(Comparator.comparingInt(Map.Entry::getValue))
+        getOrderedQueue(leastFreqWords)
                 .forEach((entry) -> System.out.printf("%s - %d раз(а)\n", entry.getKey(), entry.getValue()));
-        //(O(10log10) + 10) ~43 т.к. в очереди максимум 10 слов, сложность сортировки nlogn + итерация по элементам
+        //(O(10log10) + 10) ~43 т.к. 10log10 у функции getOrderedQueue + 10 на итерацию или O(1)
 
-        //Итоговая сложность O(n + 6n + 6s  + 43 + 43) -> O(7n + 6S + 86) или O(n)
+        //Итоговая сложность O(n + 4n + 4s  + 43 + 43) -> O(5n + 4S + 86) или O(n)
+    }
+
+    private static List<Map.Entry<String, Integer>> getOrderedQueue(Queue<Map.Entry<String, Integer>> queue) {
+        List<Map.Entry<String, Integer>> result = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            result.addFirst(queue.poll());
+        }
+        return result;
+        // 10log10 т.к. isEmpty и addFirst - O(1), у poll - O(logn), где n - кол-во элементов (max 10), итераций так же максимум 10
     }
 }
